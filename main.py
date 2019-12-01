4c9f6ca2bfe177478c61eda712e6d8befbbc738a
X-I-N <s1773844883@gmail.com>
2019-12-01 11:33:28 +0800

    create_process method complete

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 class Process:
-    def __init__(self, name, priority):
+    def __init__(self, name, priority, status='ready'):
         self.name = name
         self.priority = priority
         if name == 'init':
             self.parent = None
         else:
-            self.parent = processes[count]
+            self.parent = running
+        self.status = status
+        self.child = None
 
 
 instructions = []
-processes = []
-count = 0
+processes_1 = []
+processes_2 = []
+count_1 = 0
+count_2 = 0
+blocked_list = []
+running = None
 
 
 def init():
     print("Process init is running ......")
     pro = Process('init', 0)
-    processes.append(pro)
+    pro.status = 'running'
+    global running
+    running = pro
     print("> init", end=' ')
 
 
@@ -29,8 +37,36 @@ def get_instructions(file_path):
 
 def execute(instruction):
     type_ins = instruction.split()[0]
-    print(type_ins)
-    pass
+    if type_ins == 'cr':
+        name = instruction.split()[1]
+        priority = instruction.split()[2]
+        create_process(name, int(priority))
+        schedule()
+    elif type_ins == 'to':
+        pass
+
+
+def create_process(name, priority):
+    pro = Process(name, priority)
+    running.child = pro
+    if priority == 1:
+        processes_1.append(pro)
+    elif priority == 2:
+        processes_2.append(pro)
+
+
+def schedule():
+    global running
+    run_priority = running.priority
+    if len(processes_2) != 0 and run_priority < 2:
+        running.status = 'ready'
+        processes_2[count_2].status = 'running'
+        running = processes_2[count_2]
+    elif len(processes_1) != 0 and run_priority < 1:
+        running.status = 'ready'
+        processes_1[count_1].status = 'running'
+        running = processes_1[count_1]
+    print(running.name, end=' ')
 
 
 if __name__ == '__main__':
